517d26a88cca1378db757712540660375b02444c
Yureva Lyubov <yureva.exceedteam@gmail.com>
2022-04-19 14:30:45 +0300

    Added router creating order. Fix authenticate token

--- src/Modules/Routers/medicine.router.js
+++ src/Modules/Routers/medicine.router.js
@@ -2,11 +2,18 @@ const Router = require('express');
 const router = new Router();
 const { authenticateToken } = require('../Middleware/authenticateToken.middleware');
 
-const { createUser, authUser, allOrders, allDoctors } = require('../Controllers/medicine.controller');
+const {
+  createUser,
+  authUser,
+  allDoctors
+} = require('../Controllers/medicine.controller');
+
+const { allOrders, createOrder } = require('../Controllers/orders.controller');
 
 router.post('/createUser', createUser);
 router.post('/authUser', authUser);
 router.get('/allOrders', authenticateToken, allOrders);
-router.get('/allDoctors', authenticateToken, allDoctors);
+router.get('/allDoctors', allDoctors);
+router.post('/createOrder', authenticateToken, createOrder);
 
 module.exports = router;
