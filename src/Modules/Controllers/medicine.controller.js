0747634b696cc68b7bd95f3949b35f0d1d1298d5
Yureva Lyubov <yureva.exceedteam@gmail.com>
2022-04-21 18:14:34 +0300

    Added cors

--- src/Modules/Controllers/medicine.controller.js
+++ src/Modules/Controllers/medicine.controller.js
@@ -8,11 +8,12 @@ module.exports.createUser = async (req, res) => {
     let { password } = req.body;
     if (!(login && password)) res.status(404).send("Error! Params not found!");
 
-    const salt = bcrypt.genSaltSync(process.env.SALT);
+    const salt = bcrypt.genSaltSync(10);
     password = bcrypt.hashSync(password, salt);
 
-    const newPerson = await db.query(`INSERT INTO users (login, password) values ($1, $2) RETURNING *`, [login, password]);
-    res.send(newPerson.rows[0]);
+    const user = await db.query(`INSERT INTO users (login, password) values ($1, $2) RETURNING *`, [login, password]);
+    const token = generateAccessToken({ id: user.id });
+    res.send(token);
   } catch (e) {
     res.status(422).send({ e, message: 'Error! Params not correct!' });
   }
