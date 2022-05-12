7ebe874a480ce7028fd9ad26433e5d439fc4343e
Yureva Lyubov <yureva.exceedteam@gmail.com>
2022-05-12 12:32:37 +0300

    fix expiresIn in geterate tokens

--- src/Modules/Middleware/generateToken.middleware.js
+++ src/Modules/Middleware/generateToken.middleware.js
@@ -6,10 +6,10 @@ dotenv.config();
 module.exports.generateTokens = (id) => {
   return {
     access_token: jwt.sign(id, process.env.TOKEN_ACC_SECRET, {
-      expiresIn: '15s',
+      expiresIn: '15m',
     }),
     refresh_token: jwt.sign(id, process.env.TOKEN_RFR_SECRET, {
-      expiresIn: '30s',
+      expiresIn: '10d',
     })
   }
 };
